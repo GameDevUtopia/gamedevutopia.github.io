f65d764f14473ea31f99511d176620e26931e6c3
Kiran C. Kankariya <kirankankariya097+git@gmail.com>
2024-01-20 12:52:50 +0530

    Update logo and social media links

--- src/Components/SocialFollow.js
+++ src/Components/SocialFollow.js
@@ -11,24 +11,30 @@ export default function SocialFollow() {
   return (
     <div className="social-container">
       <h3>Follow Us On</h3>
-      <a href="https://www.instagram.com/gamedevutopia/"
+      <a href="https://www.instagram.com/_gamedevutopia_/"
         className="instagram social">
         <FontAwesomeIcon icon={faInstagram} size="2x" />
-        <p>PICT</p>
+        <p>GDU</p>
       </a>
       <a href="https://www.linkedin.com/company/gamedevutopia/"
         className="linkedin social">
         <FontAwesomeIcon icon={faLinkedin} size="2x" />
         <p>LinkedIn</p>
       </a>
-      <a href="https://www.instagram.com/gamedevutopia_iiitk/" className="instagram social">
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-        <p>IIITK</p>
-      </a>
       {/* <a href="https://www.youtube.com/@gamedevutopia3007/"
         className="youtube social">
         <FontAwesomeIcon icon={faYoutube} size="2x" />
         <p>Youtube</p>
+      </a> */}
+      <h3 className="mt-3">Our Branches</h3>
+      <a href="https://www.instagram.com/gamedevutopia/"
+        className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+        <p>GDU PICT</p>
+      </a>
+      {/* <a href="https://www.instagram.com/_gamedevutopia_/" className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+        <p>GDU IITK</p>
       </a> */}
     </div>
   );
